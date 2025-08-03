459cefe7a51f1076c7178eed9eb6e6d5a7ab43d0
Shivank Chhaya <shivank.chhaya@gmail.com>
2025-08-03 09:59:18 -0700

    updated script paths

--- compsci/script.js
+++ compsci/script.js
@@ -1,7 +1,7 @@
 // Load header and nav dynamically
-fetch('header.html').then(res => res.text()).then(data => {
+fetch('/compsci/header.html').then(res => res.text()).then(data => {
     document.getElementById('header-placeholder').innerHTML = data;
 });
-fetch('nav.html').then(res => res.text()).then(data => {
+fetch('/compsci/nav.html').then(res => res.text()).then(data => {
     document.getElementById('nav-placeholder').innerHTML = data;
 });
